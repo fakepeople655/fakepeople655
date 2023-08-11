4ec86ba74b9f37ee4c432ae388495bba673c2582
Muhamad Fauzi <140620236+fauziif@users.noreply.github.com>
2023-08-11 22:13:55 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,12 +12,12 @@ print("+ Tanggal Lahir :", Tanggal_lahir, )
 print("+ Alamat :", Alamat,)
 print("+ Kelas :", Kelas,)
 print("+ Hello",Nama,"Sayang bagaimana harimu hari ini?",
-      "kalau hari ini buruk masi ada esok dan lusa untuk",
-      "mendapatkan kebahagiaan,","aku Fauzi akan selalu ada disisi kamu,",
-      "aku sayang kamu aku janji ga akan ninggalin kamu,",
-      "kamu adalah rumahku, tempat pulangku yang ternyaman,",
-      "aku cowo yang beruntung mendapatkan kamu,",
-      "apapun yang terjadi aku ga akan ninggalin kamu, kecuali perslingkuhan,",
-      "aku berharap kita bisa sampe nikah:),",
-      "I VERY LOVE YOU ARINDA PETUNIA HAZURA<3")
+        "kalau hari ini buruk masi ada esok dan lusa untuk",
+        "mendapatkan kebahagiaan,","aku Fauzi akan selalu ada disisi kamu,",
+        "aku sayang kamu aku janji ga akan ninggalin kamu,",
+        "kamu adalah rumahku, tempat pulangku yang ternyaman,",
+        "aku cowo yang beruntung mendapatkan kamu,",
+        "apapun yang terjadi aku ga akan ninggalin kamu, kecuali perslingkuhan,",
+        "aku berharap kita bisa sampe nikah:),",
+        "I VERY LOVE YOU ARINDA PETUNIA HAZURA<3")
 print("+======================================================================+")
