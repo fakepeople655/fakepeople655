221ede8434974e5427c627431aea31a76b1db1bb
fakepeople655 <140620236+fakepeople655@users.noreply.github.com>
2023-08-11 20:28:31 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+# input data
+
+Nama = input("Masuka Nama :")
+Tanggal_lahir = input("Masukan Tanggal Lahir :")
+Alamat = input("Masukan Alamat :")
+Kelas = input("Kelas :")
+
+# mengambil output
+print("+======================================================================+")
+print("+ Nama :", Nama,)               
+print("+ Tanggal Lahir :", Tanggal_lahir, )
+print("+ Alamat :", Alamat,)
+print("+ Kelas :", Kelas,)
+print("+ Hello",Nama,"Sayang bagaimana harimu hari ini?",
+      "kalau hari ini buruk masi ada esok dan lusa untuk",
+      "mendapatkan kebahagiaan,","aku Fauzi akan selalu ada disisi kamu,",
+      "aku sayang kamu aku janji ga akan ninggalin kamu,",
+      "kamu adalah rumahku, tempat pulangku yang ternyaman,",
+      "aku cowo yang beruntung mendapatkan kamu,",
+      "apapun yang terjadi aku ga akan ninggalin kamu, kecuali perslingkuhan,",
+      "aku berharap kita bisa sampe nikah:),",
+      "I VERY LOVE YOU ARINDA PETUNIA HAZURA<3")
+print("+======================================================================+")
